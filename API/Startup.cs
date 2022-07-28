0205d2d3ee6e8d6a513ee9807bd9fc66a1c1a69e
Ibrahim <mamedovarbeiter@gmail.com>
2022-07-28 12:59:36 +0300

    adding the repository methods

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,13 +6,14 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Data;
+using Core.Interfaces;
 
 namespace API
 {
     public class Startup
     {
 
-				private readonly IConfiguration _config;
+		private readonly IConfiguration _config;
 
         public Startup(IConfiguration config)
         {
@@ -21,17 +22,16 @@ namespace API
 
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
-						services.AddDbContext<StoreContext>(x => 
-								x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-		        services.AddSwaggerGen(c =>
+			services.AddDbContext<StoreContext>(x => 
+				x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+		    services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
