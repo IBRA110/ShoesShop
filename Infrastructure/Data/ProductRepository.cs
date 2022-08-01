37e9cc7fdee37bdceef8a85f96d2428d0b111ae6
Ibrahim <mamedovarbeiter@gmail.com>
2022-08-01 15:54:11 +0300

    added brands and types to HttpGet

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -6,7 +6,7 @@ using Core.Interfaces;
 
 namespace Infrastructure.Data
 {
-    public class ProductRepository :IProductRepository
+    public class ProductRepository : IProductRepository
     {
         private readonly StoreContext _context;
         public ProductRepository(StoreContext context)
@@ -15,11 +15,27 @@ namespace Infrastructure.Data
         }
         public async Task<Product> GetProductByIdAsync(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .FirstOrDefaultAsync(p => p.Id == id);
         }
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
-            return await _context.Products.ToListAsync();
+            return await _context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
+        {
+            return await _context.ProductBrands.ToListAsync();
+        }
+        
+        public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
+        {
+            return await _context.ProductTypes.ToListAsync();
         }
     }
 }
