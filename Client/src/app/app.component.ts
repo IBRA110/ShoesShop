5a70004f5cb66fb538f758099befa503bd532d71
Ibrahim <mamedovarbeiter@gmail.com>
2022-08-25 18:20:05 +0300

    get data from the server

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './interfaces/product';
+import { Pagination } from './interfaces/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Nike';
+export class AppComponent implements OnInit {
+  
+	title = 'Nike';
+	public products: Product[];
+
+	constructor(private http: HttpClient) {}
+
+	ngOnInit(): void {
+		this.http.get('https://localhost:5001/api/products?pageSize=50')
+		.subscribe((response: Pagination) => {
+			this.products = response.data;
+			console.log(response);
+		}, error => {
+			console.log(error);
+		});
+	}
 }
